18338ebbe4fe6b4a2f6295256a4f0304a13f7430
haz8989 <mccormack_harry@hotmail.com>
2021-05-28 15:01:12 +0100

    Change window icon path

--- desktop/src/me/hazedev/shooter/desktop/DesktopLauncher.java
+++ desktop/src/me/hazedev/shooter/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("Birdseye Shooter");
-		config.setWindowIcon(Assets.Paths.SHOOTER);
+		config.setWindowIcon(Assets.Paths.ARROW);
 		new Lwjgl3Application(new BirdseyeShooter(), config);
 	}
 }
