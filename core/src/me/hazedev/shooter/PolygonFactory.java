886f2a8c935bade27ae411b2c01a2c6b9edda882
haz8989 <mccormack_harry@hotmail.com>
2021-05-28 15:02:04 +0100

    Update PolygonFactory

--- core/src/me/hazedev/shooter/PolygonFactory.java
+++ core/src/me/hazedev/shooter/PolygonFactory.java
@@ -5,11 +5,42 @@ import com.badlogic.gdx.math.Polygon;
 public class PolygonFactory {
 
     public static Polygon getHexagon() {
-        return new Polygon(new float[]{6,0, 0,10, 6,20, 18,20, 24,10, 18,0});
+        return new Polygon(new float[]{
+                6, 0,
+                0, 10,
+                6, 20,
+                18, 20,
+                24, 10,
+                18, 0
+        });
     }
 
+    public static Polygon getOctagon() {
+        return new Polygon(new float[]{
+                0,4,
+                0,12,
+                4,16,
+                12,16,
+                16,12,
+                16,4,
+                12,0,
+                4,0
+
+        });
+    }
+
+
     public static Polygon getArrow() {
-        return new Polygon(new float[]{0,0, 0,5, 3,8, 0,11, 0,16, 3,16, 16,8, 3,0});
+        return new Polygon(new float[]{
+                0, 0,
+                0, 5,
+                3, 8,
+                0, 11,
+                0, 16,
+                3, 16,
+                16, 8,
+                3, 0
+        });
     }
 
 }
