908dbb4c5c4fdca0bb536107432a107bb90d5a7a
haz8989 <mccormack_harry@hotmail.com>
2021-05-28 15:15:23 +0100

    Additional Constructors for MovementComponent

--- core/src/me/hazedev/shooter/component/MovementComponent.java
+++ core/src/me/hazedev/shooter/component/MovementComponent.java
@@ -5,6 +5,24 @@ import com.badlogic.gdx.math.Vector2;
 
 public class MovementComponent implements Component {
 
+    public final Vector2 velocity;
+    public final Vector2 acceleration;
+    public float minVelocity = 0;
+    public float maxVelocity;
+
+    public MovementComponent(Vector2 velocity, Vector2 acceleration, float minVelocity, float maxVelocity) {
+        this.velocity = velocity;
+        this.acceleration = acceleration;
+        this.minVelocity = minVelocity;
+        this.maxVelocity = maxVelocity;
+    }
+
+    public MovementComponent(Vector2 velocity, Vector2 acceleration, float maxVelocity) {
+        this.velocity = velocity;
+        this.acceleration = acceleration;
+        this.maxVelocity = maxVelocity;
+    }
+
     public MovementComponent(Vector2 velocity, Vector2 acceleration) {
         this.velocity = velocity;
         this.acceleration = acceleration;
@@ -15,13 +33,14 @@ public class MovementComponent implements Component {
         this(velocity, new Vector2());
     }
 
+    public MovementComponent(float maxVelocity) {
+        this.velocity = new Vector2();
+        this.acceleration = new Vector2();
+        this.maxVelocity = maxVelocity;
+    }
+
     public MovementComponent() {
         this(new Vector2(), new Vector2());
     }
-
-    public final Vector2 velocity;
-    public final Vector2 acceleration;
-    public float minVelocity = 0;
-    public float maxVelocity;
 
 }
